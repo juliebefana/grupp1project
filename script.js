d81b683c3d18150eda2da1c3ac4e69d0f974116f
Frida-git-ux <frida.mattsson@hotmail.com>
2026-09-18 09:34:36 +0200

    -

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 // Hjärt-funktion//
 
-// 1. Letar upp alla element på sidan som har klassen "heart-icon"
+
 const hearts = document.querySelectorAll('.heart-icon');
 
-// 2. Loopar igenom dem så att koden gäller för ALLA fyra hjärtan
+
 hearts.forEach(heart => {
     
-    // 3. Skapar en funktion som lyssnar efter ett mus-klick
+    
     heart.addEventListener('click', function(event) {
         
-        // 4. Stoppar länken! Säger åt webbläsaren att stanna kvar på sidan 
-        // och inte öppna den nya sidan när man klickar på just hjärtat.
+       
         event.preventDefault(); 
         
-        // 5. Strömbrytaren: Växlar mellan ifyllt (lägger till klassen "liked") 
-        // och bara kontur (tar bort klassen "liked") vid varje klick.
+       
         this.classList.toggle('empty'); 
     });
 });
